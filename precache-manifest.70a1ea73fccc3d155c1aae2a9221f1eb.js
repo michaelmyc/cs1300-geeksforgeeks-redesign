360f5696799507df191fd2c25204bae1218f26db
Michael Mao <michael.mao99@gmail.com>
2019-10-18 05:11:22 -0400

    Updates

---
+++ precache-manifest.70a1ea73fccc3d155c1aae2a9221f1eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc4a9b70091eae55db6a11c279534ee",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "63c5a952c13906b4f8d0",
+    "url": "/react-gh-pages/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e4914cbe67b34be43dbf",
+    "url": "/react-gh-pages/static/css/main.4ae2e27c.chunk.css"
+  },
+  {
+    "revision": "63c5a952c13906b4f8d0",
+    "url": "/react-gh-pages/static/js/2.7a4857f9.chunk.js"
+  },
+  {
+    "revision": "e4914cbe67b34be43dbf",
+    "url": "/react-gh-pages/static/js/main.afeb30a5.chunk.js"
+  },
+  {
+    "revision": "daa4ed1e9acabe501205",
+    "url": "/react-gh-pages/static/js/runtime-main.21619ab4.js"
+  }
+]);
